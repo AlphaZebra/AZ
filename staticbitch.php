b03cb7911bfa323e820b97e9d2431410dc5aeab8
Robert Richardson <robert@peakzebra.com>
2021-05-30 12:42:40 -0400

    string slinging

--- staticbitch.php
+++ staticbitch.php
@@ -13,33 +13,82 @@ Author URI:
 */
 
 define('PZ_WP_URL', "http://peakzebra.link/");
+define('PZ_UNDER_DIR', "UNDER");
 
 
 $url_list = array();
+$seen = array();
 array_push($url_list, PZ_WP_URL);
-$out_html = '';
-
-while( $url = array_pop($url_list)) {
-    $html = file_get_contents($url);
-    $cnt = strlen($html);
-    $pieces = explode("<a", $html);
-    foreach($pieces as $piece) {
-        $out_html = $out_html . $piece . '<a';
-    }
-
-$out_html = substr($out_html, 0, -2);
-file_put_contents("bobby.html", $out_html);
 
 
-$perfect = strcmp( $html, $out_html);
-   
-
-   
-
+// takes a string of arbitrary text with a link url 
+// embedded in it and returns just the url
+// if no url, returns null
+function get_clean_url($chunk) {
+    $pos1 = $pos2 = 0;
+    // if not a link to another domain
+    $upstr = strtoupper($chunk);
+    // if( strpos( $chunk, PZ_UNDER_DIR ) < 1) return null;
+    // else {
+        // get position of HREF= and of closing quotation of URL that follows HREF=
+        $startpos = strpos($chunk, "HREF=");
+        $endpos = strpos($chunk, ">", $pos1);
+        $cleanurl = substr($chunk, $pos1+5, $pos2-1);
+        return $cleanurl;
+    // }
+    //      return the url
+    // else return null
 
 }
 
-$other = $html;
+function write_static_file($url) {
+    global $url_list;
+    $out_html = '';
+
+    $html = file_get_contents($url);
+        $cnt = strlen($html);
+        $pieces = explode("<a", $html); // explode is cool.
+       
+        foreach($pieces as $piece) {
+            // store the url from each link for later processing as its own page
+            // get_clean_url will only return relevant urls
+            $oldurl = get_clean_url($piece);
+            if( $oldurl) {
+                // if we've seen this url before, do nothing
+                // else add to "seen" list and push onto the url array
+                array_push( $url_list, $oldurl );
+            }
+            // if it's not 
+            // change the url within the piece of text if we need to
+            // if it's not a comment url
+
+            // the pieces are reassembled one by one so we have changed-out full page text
+            $out_html = $out_html . $piece . '<a';
+        }
+        // last loop has left '<a' on end of file, this clips those two characters off.
+        $out_html = substr($out_html, 0, -2);
+        
+        file_put_contents("bobby.html", $out_html);
+}
+
+function write_static_site () {
+    global $url_list;
+    while( $url = array_pop($url_list)) {
+        write_static_file($url);
+    }
+}
+
+
+write_static_site();
+
+
+   
+
+   
+
+
+
+
 
 /*
 function crawl_page($url, $depth = 5) {
